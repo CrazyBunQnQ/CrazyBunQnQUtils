9e9ac2fc8fdc58091b10bad50f6e34925c28d5e2
CrazyBunQnQ <baobao222222@qq.com>
2017-06-15 22:22:08 +0800

    Reformat code

--- src/com/crazybunqnq/main/MainTest.java
+++ src/com/crazybunqnq/main/MainTest.java
@@ -1,18 +1,18 @@
 package com.crazybunqnq.main;
 
-import java.io.File;
-
 import com.crazybunqnq.utils.GenerateAnswerSheet;
+
+import java.io.File;
 
 public class MainTest {
 
-	public static void main(String[] args) {
-		aa(11);
-	}
-	
-	private static void aa(int num) {
-		GenerateAnswerSheet.answer = GenerateAnswerSheet.getAnswer("E:" + File.separator + "笔记", "基础测试-选择题答案.txt");
-		GenerateAnswerSheet.myAnswer = GenerateAnswerSheet.getAnswer("答案.txt", num);
-		GenerateAnswerSheet.gradeTest("判卷结果.md", num, false);
-	}
+    public static void main(String[] args) {
+        aa(11);
+    }
+
+    private static void aa(int num) {
+        GenerateAnswerSheet.answer = GenerateAnswerSheet.getAnswer("E:" + File.separator + "笔记", "基础测试-选择题答案.txt");
+        GenerateAnswerSheet.myAnswer = GenerateAnswerSheet.getAnswer("答案.txt", num);
+        GenerateAnswerSheet.gradeTest("判卷结果.md", num, false);
+    }
 }
